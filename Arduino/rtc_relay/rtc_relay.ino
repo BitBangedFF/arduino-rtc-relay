c1459240da15508db46fdc149d1b956574267111
Jon <jlamb@harbrick.com>
2017-03-26 14:15:56 -0700

    Add LCD logic

--- Arduino/rtc_relay/rtc_relay.ino
+++ Arduino/rtc_relay/rtc_relay.ino
@@ -1,49 +1,83 @@
 #include <SparkFunDS1307RTC.h>
 #include <Wire.h>
+#include <SoftwareSerial.h>
+#include <SerLCD.h>
 
 
 #define PIN_LED (13)
-#define PIN_SQW (2)
-#define PIN_R0 (3)
-#define PIN_R1 (4)
+#define PIN_SQW (4)
+#define PIN_LCD_TX (2)
+#define PIN_LCD_RX (3)
+#define PIN_R0 (6)
+#define PIN_R1 (7)
 
-#define LOOP_DELAY (200)
-#define STARTUP_DELAY (2000)
+#define LOOP_DELAY (200U)
+#define STARTUP_DELAY (2000U)
+
+#define LCD_RATE (9600U)
+#define LCD_BACKLIGHT (157)
 
 #define RELAY0 (0)
 #define RELAY1 (1)
+#define RELAY_COUNT (2)
 
 
 typedef struct
 {
+    uint8_t pin;
     uint8_t hour_on;
-    uint8_t hour_off;
+    uint8_t hour_off;    
 } relay_schedule_s;
 
 
-static const relay_schedule_s RELAY_SCHEDULES[2] =
+static const relay_schedule_s RELAY_SCHEDULES[RELAY_COUNT] =
 {
     {
+        .pin = PIN_R0,
         .hour_on = 8,
-        .hour_off = 19  
+        .hour_off = 19
     },
     {
+        .pin = PIN_R1,
         .hour_on = 1,
-        .hour_off = 5  
+        .hour_off = 15  
     }
 };
 
 static uint8_t last_minute = 0;
+
+SoftwareSerial SERIAL_LCD( PIN_LCD_RX, PIN_LCD_TX );
+SerLCD LCD( SERIAL_LCD );
 
 
 static void rtc_config(
         void )
 {
     rtc.begin();
-
     rtc.set24Hour( true );
-
     rtc.writeSQW( SQW_SQUARE_1 );
+}
+
+
+static void lcd_config(
+        void )
+{
+    SERIAL_LCD.begin( LCD_RATE );
+    LCD.begin();    
+    LCD.clear();
+    LCD.displayOn();
+    LCD.setBacklight( LCD_BACKLIGHT );
+}
+
+
+static void relays_config(
+        void )
+{
+    uint8_t idx;
+    for( idx = 0; idx < RELAY_COUNT; idx += 1 )
+    {
+        pinMode( RELAY_SCHEDULES[idx].pin, OUTPUT );
+    }
 }
 
 
@@ -68,17 +102,22 @@ static void led_update(
 }
 
 
-static void r0_set(
+static void relay_set(
+        const uint8_t relay,
         const bool state )
 {
-    digitalWrite( PIN_R0, !state );
+    digitalWrite( RELAY_SCHEDULES[relay].pin, !state );
 }
 
 
-static void r1_set(
+static void relays_set(
         const bool state )
 {
-    digitalWrite( PIN_R1, !state );
+    uint8_t idx;
+    for( idx = 0; idx < RELAY_COUNT; idx += 1 )
+    {
+        digitalWrite( RELAY_SCHEDULES[idx].pin, !state );
+    }
 }
 
 
@@ -104,9 +143,68 @@ static void relays_update(
         void )
 {
     const uint8_t time_hour = rtc.hour();
-    
-   r0_set( relay_schedule_get( RELAY0, time_hour ) );
-   r1_set( relay_schedule_get( RELAY1, time_hour ) );
+
+    uint8_t idx;
+    for( idx = 0; idx < RELAY_COUNT; idx += 1 )
+    {
+        relay_set( idx, relay_schedule_get( idx, time_hour ) );
+    }
+}
+
+
+static void lcd_update(
+        void )
+{
+    const uint8_t time_hour = rtc.hour();
+
+    LCD.clear();
+    LCD.setPosition( 1, 0 );
+    LCD.print( "R0: " );
+    LCD.setPosition( 1, 4 );
+    if( relay_schedule_get( RELAY0, time_hour ) == true )
+    {
+        LCD.print( "ON" );
+    }
+    else
+    {
+        LCD.print( "OFF" );
+    }
+    LCD.setPosition( 1, 9 );
+    LCD.print( "R1: " );
+    LCD.setPosition( 1, 13 );
+    if( relay_schedule_get( RELAY1, time_hour ) == true )
+    {
+        LCD.print( "ON" );
+    }
+    else
+    {
+        LCD.print( "OFF" );
+    }
+    LCD.setPosition( 2, 0 );
+    if( rtc.minute() < 10 )
+    {
+        const String time_str =
+                (String(rtc.hour()) +
+                ":0" +
+                String(rtc.minute()));
+        LCD.print( time_str );
+    }
+    else
+    {
+        const String time_str =
+                (String(rtc.hour()) +
+                ":" +
+                String(rtc.minute()));
+        LCD.print( time_str );
+    }    
+    LCD.setPosition( 2, 8 );
+    const String date_str =
+            (String(rtc.month()) +
+            "/" +
+            String(rtc.date()) +
+            "/" +
+            String(rtc.year()));
+    LCD.print( date_str );
 }
 
 
@@ -114,29 +212,33 @@ void setup( void )
 {
     pinMode( PIN_SQW, INPUT_PULLUP );
     pinMode( PIN_LED, OUTPUT );
-    pinMode( PIN_R0, OUTPUT );
-    pinMode( PIN_R1, OUTPUT );
 
-    r0_set( true );
-    r1_set( true );
+    relays_config();
+    relays_set( true );
 
     led_set( true );
+
+    lcd_config();
+    LCD.print( "startup delay" );
 
     delay( STARTUP_DELAY );
 
     // TODO - should they be kept on for a short period to prevent
     // a quick transition on multiple resets?
 
-    r0_set( false );
-    r1_set( false );
-    
+    relays_set( false );
+        
     rtc_config();
 
     while( rtc.update() == false )
     {
-        led_set( false );        
+        led_set( false );
+        LCD.clear();
+        LCD.print( "waiting for RTC" );
         delay( LOOP_DELAY );
     }
+
+    LCD.clear();
 
     led_update();
 }
@@ -147,12 +249,13 @@ void loop( void )
     led_update();
     
     rtc.update();
-
+    
     const uint8_t time_minute = rtc.minute();
 
     if( time_minute != last_minute )
     {
         relays_update();
+        lcd_update();
     }
 
     last_minute = time_minute;
